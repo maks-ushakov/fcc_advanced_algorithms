2fe8efaa00f8c010c6a37ec51d263ca7621d47c7
Maks Ushakov <ush.maksim@gmail.com>
2017-06-17 22:39:34 +0300

    Add answer to Pairwise challenge

--- pairwise/answer.js
+++ pairwise/answer.js
@@ -1,4 +1,15 @@
 function pairwise(arr, arg) {
-  return arg;
+	var usedIndexes = [];
+	var i;
+	arr.forEach(function (el, index) {
+		for( i = 0; i < index ; i++ ) {
+			if( el + arr[i] === arg && usedIndexes.indexOf(i) === -1 ) {
+				usedIndexes = usedIndexes.concat([i, index]);
+				break;
+			}
+		}
+	});
+	// sum all found indexes;
+  return usedIndexes.reduce(function(acc, curr) { return acc + curr;}, 0);
 }
 
